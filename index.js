88a680af8b4c2bd498dd92144ab5f1e962e395a6
Chirag Gupta <51075671+Hardiegogo@users.noreply.github.com>
2021-08-31 02:16:35 +0530

    Added styling and next button onClick functionality

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ var cashGiven = document.querySelector("#cash-given");
 var checkButton = document.querySelector(".btn-check");
 var numberOfNotes = document.querySelectorAll(".no-of-notes");
 var errMsg = document.querySelector(".error");
+var returnMsg=document.querySelector('.return-amount');
+var nextButton=document.querySelector('.btn-next');
+var cashArea=document.querySelector('.cash-area');
 var notes = [2000, 500, 100, 20, 10, 5, 1];
 
 function calculateExchanges(returnAmt){
+  returnMsg.innerHTML = `Return amount : ${returnAmt}`;
     
     notes.forEach((note,index)=>{
         numberOfNotes[index].innerHTML =parseInt(returnAmt/note);
         returnAmt=returnAmt%note;
     })
 }
+
+nextButton.addEventListener('click',()=>{
+
+  if(billAmount.value){
+    cashArea.style.display = "block";
+    nextButton.style.display = "none";
+  }
+  
+
+})
 
 function btnClickHandler() {
   let billAmountNum=Number(billAmount.value)
@@ -22,6 +36,7 @@ function btnClickHandler() {
     errMsg.style.display = "block";
     errMsg.innerHTML = msg;
   }
+  
   if (billAmountNum > 0) {
       
     if (cashGivenNum>=billAmountNum) {
